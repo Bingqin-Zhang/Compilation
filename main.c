0a7ab466f85071a39a836e7e4f6d14f9026a00d0
Mochi <bingqin.zhang@edu.univ-paris13.fr>
2021-04-24 21:14:34 +0200

    Correction main

--- main.c
+++ main.c
@@ -5,30 +5,21 @@
 #include <stdio.h>                              /* printf */
 #include <stdlib.h>                             /* exit */
 extern FILE  *yyin;
-#define SIZE 256
 
 //char* read_file(FILE *file);
 
 int main(int argc, char **argv){
-    if(argc<2){
-        if (yyparse()==0) {                         /* yyparse calls yylex */
-            printf("\nParsing:: syntax OK\n");      /* reached if parsing folllows the grammar */
-        }
-    }
-    else{
-        FILE *file;
-        if((file = fopen(argv[1],"r")) == NULL){
+    if(argc>1){
+        if((yyin = fopen(argv[1],"r")) == NULL){
             exit(1);
         }
-        yyin = file;
-        
-        if (yyparse()==0) {                         /* yyparse calls yylex */
-            printf("\nParsing:: syntax OK\n");      /* reached if parsing folllows the grammar */
-        }
-        fclose(file);
     }
-    
+    if (yyparse()==0) {                         /* yyparse calls yylex */
+            printf("\nParsing:: syntax OK\n");      /* reached if parsing folllows the grammar */
+    }
+
     exit(EXIT_SUCCESS);
 }
+
 
 
